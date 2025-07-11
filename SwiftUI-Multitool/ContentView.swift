2627c90ef49dd7712a5976d8ff129039923379cc
Andrej Zivkovic <soveryhappyface@gmail.com>
2025-07-10 20:18:52 -0400

    Initial Commit

---
+++ SwiftUI-Multitool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Multitool
+//
+//  Created by Andrej Zivkovic on 2025-07-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
